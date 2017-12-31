3dfac11a199b300901ef3194565b53f8f7cdcc28
Thanh Tung Nguyen <justin321261@hotmail.com>
2017-12-30 23:16:57 -0500

    added obstacles, spawners, destroyers

--- Assets/scripts/BunnyController.cs
+++ Assets/scripts/BunnyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BunnyController : MonoBehaviour {
 
@@ -10,20 +11,27 @@ public class BunnyController : MonoBehaviour {
     public float jumpForce = 100f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         this.bunnyRigidBody = GetComponent<Rigidbody2D>();
         this.bunnyAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-        if(Input.GetButtonUp("Jump"))
-        {
+	void Update ()
+    {
+        if(Input.GetButtonUp("Jump")) {
             bunnyRigidBody.AddForce(transform.up * jumpForce);
         }
         
         bunnyAnimator.SetFloat("vVelocity", Mathf.Abs(bunnyRigidBody.velocity.y));
+    }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            SceneManager.LoadScene("Game");
+        }
     }
 }
---
+++ Assets/scripts/PrefabSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+    private float nextSpawnTime = 0;
+
+    public Transform[] prefabs;
+    public float spawnRate = 1;
+    public float randomDelay = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Time.time > nextSpawnTime)
+        {
+            if(prefabs.Length > 0) {
+                Instantiate(prefabs[Random.Range(0, prefabs.Length)], transform.position, Quaternion.identity);
+            }
+           
+            nextSpawnTime = Time.time + spawnRate + Random.Range(0, randomDelay);
+        }
+	}
+}
