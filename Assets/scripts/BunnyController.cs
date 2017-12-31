dd141a5cda7dea0437376535df2f5a746de19e1a
Thanh Tung Nguyen <justin321261@hotmail.com>
2017-12-30 20:23:51 -0500

    added assets, collision, and jumping mechanic

---
+++ Assets/scripts/BunnyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+
+    private Rigidbody2D bunnyRigidBody;
+    private Animator bunnyAnimator;
+
+    public float jumpForce = 100f;
+
+	// Use this for initialization
+	void Start () {
+        this.bunnyRigidBody = GetComponent<Rigidbody2D>();
+        this.bunnyAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetButtonUp("Jump"))
+        {
+            bunnyRigidBody.AddForce(transform.up * jumpForce);
+        }
+        
+        bunnyAnimator.SetFloat("vVelocity", Mathf.Abs(bunnyRigidBody.velocity.y));
+
+    }
+}
